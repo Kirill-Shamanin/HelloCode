5a2bb271ae2371e4d2cfd95f50384551d99d52fe
Kirill Shamanin <elovgar4@gmail.com>
2023-11-28 21:08:28 +0300

    Add Example 005 IfElse

--- Example002_HelloUser/obj/Debug/net8.0/Example002_HelloUser.AssemblyInfo.cs
+++ Example002_HelloUser/obj/Debug/net8.0/Example002_HelloUser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Example002_HelloUser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71d088109b118397627fe3ebe0c4a7a0284555c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b2ff9b1479f115bee268ffcfa18670deaf2db2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Example002_HelloUser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Example002_HelloUser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
