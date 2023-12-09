1bd69bbeb2c27b847cff70f644b8c070cbff251a
Kirill Shamanin <elovgar4@gmail.com>
2023-12-09 13:51:11 +0300

    Add Example 012 Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -101,41 +101,78 @@
 
 //----------------------------------------------------
 
-// 1. Найти позицию мин эл-та в неотсортированной части массива.
+// 1. Найти позицию мин(первый код)/макс(второй код) эл-та в неотсортированной части массива.
 // 2. Произвести обмен этого значения со сначением первой неотсортированной позиции.
 // 3. Повторять пока есть неотсортированные эл-ты.
 
-int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
 
-    for (int i = 0; i < count; i ++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
+//     for (int i = 0; i < count; i ++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
 
-void SelectionSort(int[] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int minPosition = i;
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
 
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if (array[j] < array[minPosition]) minPosition = j;
-        }
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
 
-        int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
-    }
-}
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
 
-PrintArray(arr);
-SelectionSort(arr);
+// PrintArray(arr);
+// SelectionSort(arr);
 
-PrintArray(arr);
+// PrintArray(arr);
+
+
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i ++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int maxPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] > array[maxPosition]) maxPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[maxPosition];
+//         array[maxPosition] = temporary;
+//     }
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
