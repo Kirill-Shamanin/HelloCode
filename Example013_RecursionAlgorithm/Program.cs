d72d17c49f3eeb245e7e507be2d994c2809edcee
Kirill Shamanin <elovgar4@gmail.com>
2023-12-27 15:45:18 +0300

    Recursion

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -54,19 +54,32 @@
 //     Console.WriteLine($"{i}! = {Factorial(i)}");
 // }
 
+// int Fact(int n)
+
+// {
+//     if(n == 1 || n == 0)
+//     {
+//         return 1;
+//     }
+//     return n * Fact(n - 1);
+// }
+
+// Console.Write(Fact(0));
+
+
 //----------------------------------------------------------------------------
 
 // f(1) = 1
 // f(2) = 1
 // f(n) = f(n-1) + f(n-2)
 
-double Fibonacci (int n)
-{
-    if(n == 1 || n == 2) return 1;
-    else return Fibonacci(n-1) + Fibonacci(n-2);
-}
+// double Fibonacci (int n)
+// {
+//     if(n == 1 || n == 2) return 1;
+//     else return Fibonacci(n-1) + Fibonacci(n-2);
+// }
 
-for(int i = 1; i < 50; i++)
-{
-    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
-}
+// for(int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+// }
