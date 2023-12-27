db72b1032f8a2e1db511354997eb8a5387b1d549
Kirill Shamanin <elovgar4@gmail.com>
2023-12-27 15:58:14 +0300

    Recursion

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -59,12 +59,14 @@
 // {
 //     if(n == 1 || n == 0)
 //     {
+//         Console.WriteLine($"Stop: {n}");
 //         return 1;
 //     }
+//     Console.WriteLine(n);
 //     return n * Fact(n - 1);
 // }
 
-// Console.Write(Fact(0));
+// Console.Write(Fact(5));
 
 
 //----------------------------------------------------------------------------
